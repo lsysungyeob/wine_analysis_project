e32701d5708e08f9954e9d0187c5d79acec09be8
lsysungyeob <nyatane@gmail.com>
2025-02-07 12:40:29 +0900

    Python 코드 실행 및 로그 저장 shell 스크립트

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+echo 'analyze.py 실행'
 # analyze.py 실행 후, 표준 출력과 표준 에러를 모두 log.txt에 저장
 python3 analyze.py >> log.txt 2>&1
 
 # 스크립트 실행 완료 메시지 출력
-echo "analyze.py 실행이 완료되었습니다. 로그는 log.txt 파일에 저장되었습니다."
+echo 'analyze.py 실행이 완료되었습니다. 로그는 log.txt 파일에 저장되었습니다.'
