75c2badaa78dba60d99434f6e6ac119190ce832d
lsysungyeob <nyatane@gmail.com>
2025-02-07 12:26:09 +0900

    Scikit-learn 데이터 분석 Python 코드

--- analyze.py
+++ analyze.py
@@ -1,5 +1,12 @@
 from sklearn.datasets import load_wine
 import pandas as pd
+from datetime import datetime
+
+# 현재 날짜와 시간을 가져옵니다.
+now = datetime.now()
+
+# 날짜와 시간을 원하는 형식으로 포맷팅합니다.
+current_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
 # wine 데이터셋 로드
 wine = load_wine()
@@ -14,7 +21,7 @@ num_rows, num_cols = df.shape
 # 데이터셋의 특성별로 평균과 표준편차를 계산하고
 # 각 특성의 평균, 표준편차, 최대값, 최소값을 출력
 # 결과를 output.txt에 저장
-print('와인 데이터셋의 분석을 시작합니다.')
+print(f'{current_time} 와인 데이터셋의 분석을 시작합니다.')
 
 with open('output.txt', 'w') as f:
     f.write('와인 데이터셋 분석\n')
