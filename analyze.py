51f72b0903fa47991e5c387e08efe1c32ba5ffe1
lsysungyeob <nyatane@gmail.com>
2025-02-07 12:12:05 +0900

    Scikit-learn 데이터 분석 Python 코드

--- analyze.py
+++ analyze.py
@@ -21,13 +21,15 @@ with open('output.txt', 'w') as f:
     print(f'와인 데이터셋의 행의 갯수는 {num_rows}, 열의 갯수는 {num_cols}.')
     f.write(f'행: {num_rows}, 열: {num_cols}\n')
     f.write('각 특성별 정보\n')
+    f.write(f'{'feature':30}|{'mean':7}|{'std':7}|{'max':7}|{'min':7}\n')
+    f.write('-' * 62 + '\n')
     for feature in features:
         mean = df[feature].mean()
         std = df[feature].std()
         max = df[feature].max()
         min = df[feature].min()
-        print(f'{feature}의 최대값은 {max}, 최소값은 {min}.')
-        f.write(f'{feature} : 평균: {mean:.4f}, 표준편차: {std:.4f}, 최대값: {max}, 최소값: {min}\n')
+        
+        f.write(f'{feature:30}|{mean:7.2f}|{std:7.2f}|{max:7}|{min:7}\n')
     
 print('와인 데이터셋의 분석이 끝났습니다. 결과는 output.txt파일에 정장되었습니다.')
     
