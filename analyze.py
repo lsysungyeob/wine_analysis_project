de9a6fbb304851652c66164bc629d8c084b29a67
lsysungyeob <nyatane@gmail.com>
2025-02-07 11:49:32 +0900

    Scikit-learn 데이터 분석 Python 코드

---
+++ analyze.py
@@ -0,0 +1,33 @@
+from sklearn.datasets import load_wine
+import pandas as pd
+
+# wine 데이터셋 로드
+wine = load_wine()
+
+# 데이터를 DataFrame으로 변환 (특성 이름을 컬럼으로 지정)
+features = wine.feature_names
+df = pd.DataFrame(wine.data, columns=features)
+
+# 데이터셋의 행(row)과 열(column) 개수 계산
+num_rows, num_cols = df.shape
+
+# 데이터셋의 특성별로 평균과 표준편차를 계산하고
+# 각 특성의 평균, 표준편차, 최대값, 최소값을 출력
+# 결과를 output.txt에 저장
+print('와인 데이터셋의 분석을 시작합니다.')
+
+with open('output.txt', 'w') as f:
+    f.write('와인 데이터셋 분석\n')
+    print(f'와인 데이터셋의 행의 갯수는 {num_rows}, 열의 갯수는 {num_cols}.')
+    f.write(f'행: {num_rows}, 열: {num_cols}\n')
+    f.write('각 특성별 정보\n')
+    for feature in features:
+        mean = df[feature].mean()
+        std = df[feature].std()
+        max = df[feature].max()
+        min = df[feature].min()
+        print(f'{feature}의 최대값은 {max}, 최소값은 {min}.')
+        f.write(f'{feature} : 평균: {mean:.4f}, 표준편차: {std:.4f}, 최대값: {max}, 최소값: {min}\n')
+    
+print('와인 데이터셋의 분석이 끝났습니다. 결과는 output.txt파일에 정장되었습니다.')
+    
